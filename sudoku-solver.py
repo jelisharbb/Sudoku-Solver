80fa4458391dc44fcb9b2f66088a3b1c60c4c6e4
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2023-02-09 22:54:50 +0800

    Renamed the variable for better understanding.

--- sudoku-solver.py
+++ sudoku-solver.py
@@ -1,39 +1,41 @@
 import numpy as np
 
-grid = [[0, 0, 0, 2, 6, 0, 7, 0, 1], [6, 8, 0, 0, 7, 0, 0, 9, 0], [1, 9, 0, 0, 0, 4, 5, 0, 0], [8, 2, 0, 1, 0, 0, 0, 4, 7], [0, 0, 4, 6, 0, 2, 9, 0, 0], [0, 5, 0, 0, 0, 3, 0, 2, 8], [0, 0, 9, 3, 0, 0, 0, 7, 4], [0, 4, 0, 0, 5, 0, 0, 3, 6], [7, 0, 3, 0, 1, 8, 0, 0, 0]]
+puzzle = [[0, 0, 0, 2, 6, 0, 7, 0, 1], [6, 8, 0, 0, 7, 0, 0, 9, 0], [1, 9, 0, 0, 0, 4, 5, 0, 0], [8, 2, 0, 1, 0, 0, 0, 4, 7], [0, 0, 4, 6, 0, 2, 9, 0, 0], [0, 5, 0, 0, 0, 3, 0, 2, 8], [0, 0, 9, 3, 0, 0, 0, 7, 4], [0, 4, 0, 0, 5, 0, 0, 3, 6], [7, 0, 3, 0, 1, 8, 0, 0, 0]]
 
-print(np.matrix(grid))
+print(np.matrix(puzzle))
 
-def possible(y, x, n):
-    global grid
+def possible(row, column, answer):
+    global puzzle
+
     for i in range(0, 9):
-        if grid [y][i] == n:
+        if puzzle [row][i] == answer:
             return False
+
     for i in range(0, 9):
-        if grid [i][x] == n:
+        if puzzle [i][column] == answer:
             return False
-    x0 = (x//3)*3
-    y0 = (y//3)*3
+
+    subBoxRow = (row//3)*3
+    subBoxCol = (column//3)*3
     for i in range(0, 3):
         for j in range(0, 3):
-            if grid[y0+1][x0+j] == n:
+            if puzzle[subBoxRow + 1][subBoxCol + j] == answer:
                 return False
+                
     return True
 
-print(possible(4, 4, 3))
-print(possible(4, 4, 5))
-
 def solve():
-    global grid
-    for y in range(9):
-        for x in range(9):
-            if grid[y][x] == 0:
-                for n in range(1, 10):
-                    if possible(y, x, n):
-                        grid[y][x] = n
+    global puzzle
+    for row in range(9):
+        for column in range(9):
+            if puzzle[row][column] == 0:
+                for answer in range(1, 10):
+                    if possible(row, column, answer):
+                        puzzle[row][column] = answer
                         solve()
-                        grid[y][x] = 0
+                        puzzle[row][column] = 0
                 return
-    print(np.matrix(grid))
+    
+    print(np.matrix(puzzle))
 
 solve()
