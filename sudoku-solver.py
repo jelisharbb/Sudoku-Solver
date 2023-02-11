7b6395dc557bbcf13bea48c2ff0eafbc5cbbba68
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2023-02-11 13:33:59 +0800

    Added 4x4 puzzles in the size option.

--- sudoku-solver.py
+++ sudoku-solver.py
@@ -18,7 +18,7 @@ subBoxDividerX = 0
 subBoxDividerY = 0
 answerRangeIndex = 0
 
-size = int(input("What size of the puzzle do you want to find a solution for? (3/6/9): "))
+size = int(input("What size of the puzzle do you want to find a solution for? (3/4/6/9): "))
 
 if size == 3:
     puzzleSize = 3
@@ -47,32 +47,40 @@ if size == 3:
     elif chosenPuzzle3 == 5:
         finalPuzzle = puzzle3_5
 
-elif size == 6:
-    puzzleSize = 6
+if size == 4:
+    puzzleSize = 4
     subBoxDividerX = 2
-    subBoxDividerY = 3
+    subBoxDividerY = 2
     subBoxMultiplierX = 2
-    subBoxMultiplierY = 3
-    answerRangeIndex = 7
+    subBoxMultiplierY = 2
+    answerRangeIndex = 5
 
-    # 6 by 6 puzzles
-    puzzle6_1 =[[0, 0, 6, 0, 1, 0], [4, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 2], [6, 0, 0, 0, 0, 0], [0, 0, 0, 0, 4, 5], [0, 2, 0, 3, 0, 0]]
-    puzzle6_2 =[[0, 0, 1, 0, 0, 0], [0, 0, 0, 6, 0, 0], [1, 0, 0, 0, 3, 0], [0, 4, 0, 0, 0, 2], [0, 0, 2, 0, 0, 0], [0, 0, 0, 2, 0, 0]]
-    puzzle6_3 =[[0, 0, 0, 1, 5, 0], [2, 5, 1, 0, 3, 0], [1, 0, 2, 3, 4, 0], [0, 3, 5, 2, 0, 6], [0, 1, 0, 4, 2, 3], [0, 2, 4, 0, 0, 0]]
-    puzzle6_4 =[[1, 0, 0, 4, 6, 3], [3, 6, 4, 0, 2, 0], [5, 0, 0, 0, 4, 0], [0, 2, 0, 0, 0, 1], [0, 4, 0, 5, 3, 2], [2, 3, 5, 0, 0, 4]]
-    puzzle6_5 =[[1, 0, 4, 6, 0, 5], [0, 0, 0, 1, 0, 0], [6, 1, 5, 4, 0, 2], [3, 0, 2, 5, 6, 1], [0, 0, 3, 0, 0, 0], [2, 0, 1, 3, 0, 4]]
+# elif size == 6:
+#     puzzleSize = 6
+#     subBoxDividerX = 2
+#     subBoxDividerY = 3
+#     subBoxMultiplierX = 2
+#     subBoxMultiplierY = 3
+#     answerRangeIndex = 7
 
-    chosenPuzzle6 = int(input("Which among the 6x6 puzzle do you want to find a solution for? (1/2/3/4/5): ")) 
-    if chosenPuzzle6 == 1:
-        finalPuzzle = puzzle6_1
-    elif chosenPuzzle6 == 2:
-        finalPuzzle = puzzle6_2
-    elif chosenPuzzle6 == 3:
-        finalPuzzle = puzzle6_3
-    elif chosenPuzzle6 == 4:
-        finalPuzzle = puzzle6_4
-    elif chosenPuzzle6 == 5:
-        finalPuzzle = puzzle6_5
+#     # 6 by 6 puzzles
+#     puzzle6_1 =[[0, 0, 6, 0, 1, 0], [4, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 2], [6, 0, 0, 0, 0, 0], [0, 0, 0, 0, 4, 5], [0, 2, 0, 3, 0, 0]]
+#     puzzle6_2 =[[0, 0, 1, 0, 0, 0], [0, 0, 0, 6, 0, 0], [1, 0, 0, 0, 3, 0], [0, 4, 0, 0, 0, 2], [0, 0, 2, 0, 0, 0], [0, 0, 0, 2, 0, 0]]
+#     puzzle6_3 =[[0, 0, 0, 1, 5, 0], [2, 5, 1, 0, 3, 0], [1, 0, 2, 3, 4, 0], [0, 3, 5, 2, 0, 6], [0, 1, 0, 4, 2, 3], [0, 2, 4, 0, 0, 0]]
+#     puzzle6_4 =[[1, 0, 0, 4, 6, 3], [3, 6, 4, 0, 2, 0], [5, 0, 0, 0, 4, 0], [0, 2, 0, 0, 0, 1], [0, 4, 0, 5, 3, 2], [2, 3, 5, 0, 0, 4]]
+#     puzzle6_5 =[[1, 0, 4, 6, 0, 5], [0, 0, 0, 1, 0, 0], [6, 1, 5, 4, 0, 2], [3, 0, 2, 5, 6, 1], [0, 0, 3, 0, 0, 0], [2, 0, 1, 3, 0, 4]]
+
+#     chosenPuzzle6 = int(input("Which among the 6x6 puzzle do you want to find a solution for? (1/2/3/4/5): ")) 
+#     if chosenPuzzle6 == 1:
+#         finalPuzzle = puzzle6_1
+#     elif chosenPuzzle6 == 2:
+#         finalPuzzle = puzzle6_2
+#     elif chosenPuzzle6 == 3:
+#         finalPuzzle = puzzle6_3
+#     elif chosenPuzzle6 == 4:
+#         finalPuzzle = puzzle6_4
+#     elif chosenPuzzle6 == 5:
+#         finalPuzzle = puzzle6_5
 
 elif size == 9:
     puzzleSize = 9
